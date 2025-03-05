b3909f0537b9d4d53ea6c81e410fea5cda2f10e2
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-03-05 22:28:31 +0300

    edit config pm2

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,12 +1,14 @@
-module.exports = {
-  apps: [
-    {
-      name: 'qr-order-client',
-      script: 'node_modules/next/dist/bin/next',
-      args: '-p 4000',
-      cwd: './client',
-      instances: '2',
-      exec_mode: 'cluster'
-    }
-  ]
-}
+export const apps = [
+  {
+    name: 'qr-order-client',
+    script: 'npm',
+    args: 'run start',
+    instances: '2',
+    exec_mode: 'cluster',
+
+    out_file: './logs/server-out.log',
+    error_file: './logs/server-error.log',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm Z'
+  }
+]
