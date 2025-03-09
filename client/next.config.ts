b26d0266f66e5c0cfdd2219b9fb6bdedd8ebe310
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-03-10 01:04:54 +0300

    add config image

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,27 +1,31 @@
-import NextBundleAnalyzer from "@next/bundle-analyzer";
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import NextBundleAnalyzer from '@next/bundle-analyzer'
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "localhost",
-        pathname: "/**",
+        hostname: 'localhost',
+        pathname: '/**'
       },
       {
-        hostname: "api-bigboy.duthanhduoc.com",
-        pathname: "/**",
+        hostname: 'api-bigboy.duthanhduoc.com',
+        pathname: '/**'
       },
       {
-        hostname: "via.placeholder.com",
-        pathname: "/**",
+        hostname: 'via.placeholder.com',
+        pathname: '/**'
       },
-    ],
-  },
-};
+      {
+        hostname: '164181.msk.web.highserver.ru',
+        pathname: '/**'
+      }
+    ]
+  }
+}
 const withBundleAnalyzer = NextBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-export default withBundleAnalyzer(withNextIntl(nextConfig));
+  enabled: process.env.ANALYZE === 'true'
+})
+export default withBundleAnalyzer(withNextIntl(nextConfig))
