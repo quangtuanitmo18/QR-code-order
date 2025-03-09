5943d807725d8890d55c58b80cfe6399b43bf950
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-03-09 16:06:39 +0300

    edit pm2 config file

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,14 +1,16 @@
-export const apps = [
-  {
-    name: 'qr-order-client',
-    script: 'npm',
-    args: 'run start',
-    instances: '2',
-    exec_mode: 'cluster',
+module.exports = {
+  apps: [
+    {
+      name: 'qr-order-client',
+      script: 'npm',
+      args: 'run start',
+      instances: '2',
+      exec_mode: 'cluster',
 
-    out_file: './logs/server-out.log',
-    error_file: './logs/server-error.log',
-    merge_logs: true,
-    log_date_format: 'YYYY-MM-DD HH:mm Z'
-  }
-]
+      out_file: './logs/server-out.log',
+      error_file: './logs/server-error.log',
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm Z'
+    }
+  ]
+}
