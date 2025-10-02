bd813da7febb2dbc8ec81c25b18f0a30a3b26aa9
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-10-02 18:21:38 +0300

    change config sentry

--- client/instrumentation.ts
+++ client/instrumentation.ts
@@ -1,6 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
 export const onRequestError = Sentry.captureRequestError;
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
