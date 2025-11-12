195a742c270dbf03304201a958b4e1fc50c2f4f1
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2025-11-13 02:53:32 +0300

    chore: fix cicd

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -17,9 +17,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-/**
- * Xóa đi ký tự `/` đầu tiên của path
- */
 export const normalizePath = (path: string) => {
   return path.startsWith('/') ? path.slice(1) : path
 }
